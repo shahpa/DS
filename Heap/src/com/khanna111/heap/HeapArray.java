29c4af1f871778ec1e2a0bc5e32007e62a03bc9b
khanna111 <gk2@hotmail.com>
2014-11-13 21:56:28 -0800

    Adding HeapArray with minimal  Floyd's algorithm plumbing

--- Heap/src/com/khanna111/heap/HeapArray.java
+++ Heap/src/com/khanna111/heap/HeapArray.java
@@ -1,5 +1,83 @@
 package com.khanna111.heap;
 
-public class HeapArray {
+/**
+ * 
+ * @author khanna
+ *
+ */
+public class HeapArray implements HeapI {
 
+    /* We have a max of 64 elements or max height of 6 */
+    private int[] heapAsAnArray; //= new int[64];
+    
+    private int getLeftChildIndex(int parentIndex) {
+	/* 2*parentIndex + 1; */
+//	return 2*parentIndex + 1;
+	return parentIndex << 1 + 1;
+    }
+    
+    private int getRightChildIndex(int parentIndex) {
+	/* 2*parentIndex + 2 */
+//	return 2*parentIndex + 2;
+	return parentIndex << 1 + 2;
+	
+    }
+    
+    private int getParentIndex (int childIndex) {
+	/* floor ((childIndex - 1 ) /2 ) */
+	return (childIndex -1 ) / 2;
+    }
+    
+    /**
+     * Using Floyd's algorithm
+     * 
+     * @param array - broken heap represented as an array
+     */
+    @Override
+    public void createHeap(int[] array) {
+	heapAsAnArray = array;
+	int count = heapAsAnArray.length;
+	
+	// get the last non-leaf element
+	int lastNonLeafIndex = (count / 2)  -1;
+	
+	// index of the first child of root
+	while (lastNonLeafIndex >=0) {
+	    siftDown(heapAsAnArray, lastNonLeafIndex, count -1);
+	    lastNonLeafIndex--;
+	}
+	
+	
+    }
+
+    private void siftDown(int[] heapArray, int lastNonLeafIndex, int lastLeaf) {
+	
+    }
+    
+    @Override
+    public void heapify() {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public int findMin() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public void insert(int i) {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public int size() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    
+    
 }
---
+++ Heap/src/com/khanna111/heap/HeapI.java
@@ -0,0 +1,17 @@
+package com.khanna111.heap;
+
+public interface HeapI {
+
+    public void createHeap(int[] array);
+    
+    public void heapify();
+    
+    public int findMin();
+    
+    
+    public void insert(int i);
+    
+    public int size();
+    
+    
+}
