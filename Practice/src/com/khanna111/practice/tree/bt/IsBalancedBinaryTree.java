98f86b8225e95ae180f7ff2d92c514c631593bd3
khanna111 <gk2@hotmail.com>
2015-04-22 22:13:43 -0700

    removing tests from regular class since it is moved to tests

--- Practice/src/com/khanna111/practice/tree/bt/IsBalancedBinaryTree.java
+++ Practice/src/com/khanna111/practice/tree/bt/IsBalancedBinaryTree.java
@@ -51,65 +51,9 @@ public class IsBalancedBinaryTree {
 	if (h > 1) {
 	    return -1;
 	} else {
-	    return Math.max(lh,rh) + 1;
+	    return Math.max(lh, rh) + 1;
 	}
 
-    }
-    
-    public static void main(String[] args) {
-	IsBalancedBinaryTree b = new IsBalancedBinaryTree();
-	
-	// we will use BST instead of a regular binary tree
-	BinarySearchTree<Integer, String> tree = new BinarySearchTree<>();
-	tree.put(50, "");
-	tree.put(20, "");
-	tree.put(70, "");
-	tree.put(10, "");
-	tree.put(30, "");
-	tree.put(60, "");
-	tree.put(80, "");
-	tree.put(6, "");
-//	tree.put(11, "");
-//	tree.put(25, "");
-//	tree.put(35, "");
-//	tree.put(55, "");
-//	tree.put(65, "");
-//	tree.put(3, "");
-//	tree.put(22, "");
-//	tree.put(33, "");
-//	tree.put(36, "");
-//	tree.put(5, "");
-//	tree.put(21, "");
-//	tree.put(23, "");
-//	tree.put(4, "");
-//	tree.put(24, "");
-//	tree.put(85, "");
-//	tree.put(79, "");
-	
-	tree.print();
-	
-	
-	int isBalanced = b.isBalanced(tree.getRoot());
-	System.out.println("Tree is balanced : " + (isBalanced == -1 ? false : true));
-	
-	tree.put(3, "");
-//	tree.put(22, "");
-//	tree.put(33, "");
-//	tree.put(36, "");
-	tree.put(5, "");
-//	tree.put(21, "");
-//	tree.put(23, "");
-//	tree.put(4, "");
-//	tree.put(24, "");
-//	tree.put(85, "");
-//	tree.put(79, "");
-	
-	tree.print();
-	
-	isBalanced = b.isBalanced(tree.getRoot());
-	System.out.println("Tree is balanced : " + (isBalanced == -1 ? false : true));
-
-	
     }
 
 }
